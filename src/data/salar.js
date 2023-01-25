29e2853adcfe2a047a37bd02b03618500550d1c9
Salar Hafezi <salar.hfz.work@gmail.com>
2023-01-25 14:22:12 +0330

    add salar info

---
+++ src/data/salar.js
@@ -0,0 +1,27 @@
+const salarInfo = {
+    general: {
+        fullName: 'Salar Hafezi',
+        profession: 'Full Stack JavaScript Engineer',
+        website: 'salarhafezi.ir',
+        email: 'salar.hfz.work@gmail.com',
+    },
+    about: `I am a full stack developer with a particular interest in making things simple and automating daily tasks.
+    I try to keep up with security and best practices, and am always looking for new things to learn.`,
+    interests: `Food | Table tennis | Tech books | Travel`,
+    socialHandles: [
+        {
+            title: 'Twitter', iconUrl: '/twitter-icon.svg', url: 'https://twitter.com/salar_hafezi',
+        },
+        {
+            title: 'Instagram', iconUrl: '/instagram-icon.svg', url: 'https://www.instagram.com/salarhafezi/?igshid=ZDdkNTZiNTM%3D',
+        },
+        {
+            title: 'LinkedIn', iconUrl: '/linkedin-icon.svg', url: 'https://www.linkedin.com/in/salarhafezi',
+        },
+        {
+            title: 'Github', iconUrl: '/github-icon.svg', url: 'https://github.com/salarhfz-fs',
+        },
+    ],
+};
+
+export default salarInfo;
